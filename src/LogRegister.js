f6665ed6bc6c0e5b38420999b4c5dd235a693b49
wojcikewelina <gargula.ewe@gmail.com>
2020-09-28 09:42:10 +0200

    1 file modified - password confirmation validation

--- src/LogRegister.js
+++ src/LogRegister.js
@@ -1,25 +1,61 @@
 import React, { Component } from "react";
 
 export default class LogRegister extends Component {
-  render() {
-
-
-    handleLogInSubmit=(event)=>{
-      
+  constructor(props) {
+    super(props);
+    this.state = {
+      status : "your not logged in",
     }
+  }
 
+  clearRegisterPasword = () => {
+    event.target.userPassword.value = "";
+    event.target.userPasswordToConfirm.value = "";
+  };
+
+  handleLogInSubmit = event => {
+    event.preventDefault();
+    this.logInUser(
+      event.target.userEmail.value,
+      event.target.userPassword.value
+    );
+  };
+
+  logInUser(email, password) {}
+
+  handleRegisterSubmit = event => {
+    event.preventDefault();
+    let password = event.target.userPassword.value;
+    let passwordToConfirm = event.target.userPasswordToConfirm.value;
+
+    if (password !== passwordToConfirm) {
+      alert("Passwords are not the same!");
+      this.clearRegisterPasword();
+    } else if (password.length < 6) {
+      alert("Password must have at least 6 letters");
+      this.clearRegisterPasword();
+    } else {
+      console.log("you have been registered");
+    }
+  };
+
+  render() {
     return (
       <section>
-        <div>Status</div>
+        <div>Status: {this.state.status}</div>
         <form onSubmit={this.handleLogInSubmit}>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="Email" id="userEmail" />
+          <input type="password" placeholder="Password" id="userPassword" />
           <button>Log in</button>
         </form>
-        <form>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm password" />
+        <form onSubmit={this.handleRegisterSubmit}>
+          <input type="text" placeholder="Email" id="userEmail" />
+          <input type="password" placeholder="Password" id="userPassword" />
+          <input
+            type="password"
+            placeholder="Confirm password"
+            id="userPasswordToConfirm"
+          />
           <button>Register</button>
         </form>
       </section>
